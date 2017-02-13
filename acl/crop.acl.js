cd7eac4fe397e457a15fd0201242b54b26235a5a
Nick Williams <nick22891@yahoo.com>
2017-02-13 02:28:52 -0500

    Removing restrictions on Property IDX for Crops and Livestock

--- acl/crop.acl.js
+++ acl/crop.acl.js
@@ -3,7 +3,7 @@ module.exports = {
         access: ['aggregate_user'],
         fields: {
             restricted: {
-                aggregate_user: ['Property_Address', 'IDX_Crop', 'IDX_Property']
+                aggregate_user: ['Property_Address', 'IDX_Crop']
             }
         }
     }
